68deb0a880cb0df1b18bf4a000e226504a7c3aa0
agopdev <alonsogonzalez423@gmail.com>
2024-06-17 05:16:17 -0600

    Added camera photos and video recording

--- app/(tabs)/IneTab.tsx
+++ app/(tabs)/IneTab.tsx
@@ -1,12 +1,23 @@
 import { StyleSheet } from 'react-native';
-
+import { Button } from 'react-native-paper';
 import { Text, View } from '@/components/Themed';
+import { useState } from 'react';
 
 export default function IneTab() {
+
+  const [takePhoto, setTakePhoto] = useState(false);
+
+  const handleTakePhoto = () => {
+    
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Ine Capture</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Button mode="contained" onPress={handleTakePhoto}>INE frente</Button>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Button mode="contained" onPress={handleTakePhoto}>INE reverso</Button>
     </View>
   );
 }
